7c9c9cab82e43f2c4a241de17f5451996a8bca73
dengfuming123 <dfm1998@qq.com>
2019-05-26 10:53:29 +0800

    525demo
    攀爬移动都在Fplayer里。

---
+++ 525/Assets/Feng/SceneFadeInOut.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneFadeInOut : MonoBehaviour {
+
+
+    public float fadeSpeed = 1.5f;
+    public bool sceneStarting = true;
+    private RawImage rawImage;
+    void Awake()
+    {
+        rawImage = GetComponent<RawImage>();
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (sceneStarting)
+            StartScene();
+    }
+
+    private void FadeToClear()
+    {
+        rawImage.color = Color.Lerp(rawImage.color, Color.clear, fadeSpeed * Time.deltaTime);
+    }
+
+    private void FadeToBlack()
+    {
+        rawImage.color = Color.Lerp(rawImage.color, Color.black, fadeSpeed * Time.deltaTime);
+    }
+
+    void StartScene()
+    {
+        FadeToClear();
+        if (rawImage.color.a <=0.0f)
+        {
+            rawImage.color = Color.clear;
+            rawImage.enabled = false;
+            sceneStarting = false;
+        }
+    }
+
+    //void EndScene()
+    //{
+    //    rawImage.enabled = true;
+    //    FadeToBlack();
+    //    if (rawImage.color.a > 0.95f)
+    //    {
+    //        SceneManager.LoadScene(0);
+    //    }
+    //}
+
+    
+
+
+}
---
+++ 525/Assets/Feng/Upjuan.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upjuan : MonoBehaviour {
+    public GameObject bg1;  //背景1
+    public GameObject bg2;  //背景2
+    public float y1;
+    public float y2;
+    public GameObject Player;
+    public float count = 1;
+	// Use this for initialization
+	void Start () {
+		y1= bg1.transform.localScale.y * bg1.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
+       
+        y2 = bg2.transform.localScale.y * bg2.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
+        Player = GameObject.FindWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Player.transform.position.y > bg1.transform.position.y)
+        { bg2.transform.localPosition =new Vector2( bg2.transform.position.x,bg1.transform.position.y+ y1 / 2+ y2/2); }
+        else
+        { bg2.transform.localPosition = new Vector2(bg2.transform.position.x, bg1.transform.position.y - y1 / 2 - y2 / 2); }
+        if (Player.transform.position.y > bg2.transform.position.y)
+        { bg1.transform.localPosition = new Vector2(bg1.transform.position.x, bg2.transform.position.y + y1 / 2 + y2 / 2); }
+        else
+        { bg1.transform.localPosition = new Vector2(bg1.transform.position.x, bg2.transform.position.y - y1 / 2 - y2 / 2); }
+
+    }
+}
